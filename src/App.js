281f0afc74cf5fc7c5ab275da166e52e18289777
Noeliac <ncalde01@ucm.es>
2020-11-06 18:31:46 +0100

    enlaces ok

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import web3 from 'web3';
 import lottery from './contracts/lottery';
+import web3 from '../node_modules/web3';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a href="participate()">PARTICIPAR</a>
-        <a href="chooseWinner()">PREMIO</a>
-
+class App extends Component {
+   render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
         
-      </header>
-    </div>
+          <button onClick={this.participate}>PARTICIPAR</button>
+          <button onClick={this.chooseWinner}>PREMIO</button>
+        </header>
+      </div>
     
-  );
-  
-}
-//skaldk
-async function  participate() {
-  const accounts = await web3.eth.getAccounts();
-  await lottery.methods.enter().send({
-    from: accounts[1],
-    value: 2000000000000000000
-  });
-}
-async function chooseWinner() {
-  const accounts = await web3.eth.getAccounts();
-  await lottery.methods.choseWinner().send({
-    from: accounts[0]
-  });
-}
+    );
+   
+  }
+ 
+  participate = async() =>{
+    const accounts = await web3.eth.getAccounts();
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: 2000000000000000000
+    });
+  };
+  chooseWinner = async() =>{
+    
+    const accounts = await web3.eth.getAccounts();
+    await lottery.methods.choseWinner().send({
+      from: accounts[0]
+    });
+  };
 
-
+}
 export default App;
